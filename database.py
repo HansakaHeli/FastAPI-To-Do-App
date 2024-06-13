f2b41651d8316609b6a7fb1e7726ba8455b87e46
HansakaHeli <programminghhr66@gmail.com>
2024-06-13 23:07:35 +0530

    Add comments

--- database.py
+++ database.py
@@ -1,13 +1,20 @@
-from sqlalchemy import create_engin
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engin # Import the create_engine function to establish a connection to the database
+from sqlalchemy.orm import sessionmaker # Import sessionmaker to create a new session
+from sqlalchemy.ext.declarative import declarative_base # Import declarative_base for the base class for ORM models
 
+# Database URL, replace with your actual database URL
 SQLALCHEMY_DATABASE_URL = ""
 
+# Create a SQLAlchemy engine instance
+# `connect_args={"check_same_thread": False}` is used for SQLite to allow multiple threads to interact with the database
 engin = create_engin(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
+# Create a configured "Session" class
+# autocommit=False means transactions are committed manually
+# autoflush=False means the session will not flush changes to the database automatically
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engin)
 
+# Create a Base class for our ORM models to inherit from
 Base = declarative_base()
