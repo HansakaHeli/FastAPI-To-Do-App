3de30444bc5333c3731a115e5e1c89bd4fab129b
HansakaHeli <programminghhr66@gmail.com>
2024-06-17 22:18:46 +0530

    Modify the code

--- models.py
+++ models.py
@@ -1,10 +1,14 @@
 from sqlalchemy import Boolean, Column, Integer, String
 from database import Base
 
+# Define the Todos model which maps to the "todos" table in the database
 class Todos(Base):
+
+    # Specify the table name
     __tablename__ = "todos"
 
-    id = Column(Integer, primary_key=True, index=True)
+    # Define the columns of the table
+    id = Column(Integer, primary_key=True, index=True) # Unique identifier for each todo item, primary ke, An index will be created for this column to speed up lookups.
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
